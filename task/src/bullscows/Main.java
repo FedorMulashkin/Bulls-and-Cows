6d835e479fa0563c56d1755eb48b9d7d63cf0a40
Fedor Mulashkin <fedor.mulashkin@gmail.com>
2022-02-21 20:28:48 +0300

    add checkUniqueness Method allows to create number with unique digits

--- task/src/bullscows/Main.java
+++ task/src/bullscows/Main.java
@@ -6,57 +6,69 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Please, enter the secret code's length:");
         int length = new Scanner(System.in).nextInt();
-        randomNumberGenerator(length);
-        //System.out.println(grader(secretNumber));
-    }
-
-
-
-    public static String grader(int secret) {
-        int code = new Scanner(System.in).nextInt();
-        String[] stringCode = String.valueOf(code).split("");
-        String[] secretNumber = String.valueOf(secret).split("");
-        int bulls = 0;
-        int cows = 0;
-        for (int i = 0; i < 4; i++) {
-            if (secretNumber[i].equals(stringCode[i])) {
-                bulls++;
-            } else {
-                int finalI = i;
-                if (Arrays.stream(stringCode).anyMatch(number -> number.equals(secretNumber[finalI]))) {
-                    cows++;
-                }
-            }
-        }
-        if (bulls != 0 && cows != 0){
-            return "Grade: " + bulls + " bull(s) and " + cows + " cow(s). The secret code is " + secret;
-        }else if (bulls != 0 && cows == 0){
-            return "Grade: " + bulls + " bull(s). The secret code is " + secret;
-        }else if (bulls == 0 && cows != 0){
-            return "Grade: " + cows + " cow(s). The secret code is " + secret;
-        } else{
-            return "Grade: None. the secret code is " + secret;
-        }
-    }
-
-    public static void randomNumberGenerator(int length){
-        Random random = new Random(System.nanoTime());
-        if(length > 10){
+        if (length > 10){
             System.out.println("Error: can't generate a secret number with a length of " + length
                     + " because there aren't enough unique digits.");
         }else{
-            int temp_number;
-            StringBuilder number = new StringBuilder();
-            for (int i = 0; i < length; i++) {
-                temp_number = random.nextInt(10);
-                while (!checkUniqueness(number, temp_number)){
-                    temp_number = random.nextInt(10);
-                }
-                number.append(temp_number);
-            }
-            System.out.println("The random secret number is " + number);
+            System.out.println("Okay, let's start a game!");
+            int secretNumber = randomNumberGenerator(length);
+            grader(secretNumber);
         }
+    }
+
+
+
+    public static void grader(int secret) {
+        int counter = 1;
+        while(true){
+            System.out.println("Turn " + counter + ":");
+            ++counter;
+            String code = new Scanner(System.in).next();
+            String[] stringCode = String.valueOf(code).split("");
+            String[] secretNumber = String.valueOf(secret).split("");
+            int bulls = 0;
+            int cows = 0;
+            for (int i = 0; i < secretNumber.length; i++) {
+                if (secretNumber[i].equals(stringCode[i])) {
+                    bulls++;
+                } else {
+                    int finalI = i;
+                    if (Arrays.stream(stringCode).anyMatch(number -> number.equals(secretNumber[finalI]))) {
+                        cows++;
+                    }
+                }
+            }
+            if (bulls != 0 && cows != 0){
+                System.out.println("Grade: " + bulls + " bull(s) and " + cows + " cow(s)");
+            }else if (bulls != 0 && cows == 0){
+                System.out.println("Grade: " + bulls + " bull(s).");
+                if(bulls == secretNumber.length){
+                    System.out.println("Congratulations! You guessed the secret code.");
+                    break;
+                }
+            }else if (bulls == 0 && cows != 0){
+                System.out.println("Grade: " + cows + " cow(s).");
+            } else{
+                System.out.println("Grade: None.");
+            }
+        }
+    }
+
+    public static int randomNumberGenerator(int length){
+        Random random = new Random(System.nanoTime());
+        int temp_number;
+        StringBuilder number = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            temp_number = random.nextInt(10);
+            while (!checkUniqueness(number, temp_number)){
+                temp_number = random.nextInt(10);
+            }
+            number.append(temp_number);
+        }
+        System.out.println("The random secret number is " + number);
+        return Integer.parseInt(number.toString());
     }
 
     public static boolean checkUniqueness(StringBuilder number, int newNumber){
