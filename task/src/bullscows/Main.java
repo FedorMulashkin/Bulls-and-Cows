10126a63a7faa1ba80fde85b6873ff536a98d9a4
Fedor Mulashkin <fedor.mulashkin@gmail.com>
2022-02-21 16:49:36 +0300

    add checkUniqueness Method allows to create number with unique digits

--- task/src/bullscows/Main.java
+++ task/src/bullscows/Main.java
@@ -5,10 +5,10 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         int length = new Scanner(System.in).nextInt();
-        int secretNumber = randomNumberGenerator(length);
-        System.out.println(grader(secretNumber));
+        randomNumberGenerator(length);
+        //System.out.println(grader(secretNumber));
     }
 
 
@@ -40,23 +40,40 @@ public class Main {
         }
     }
 
-    public static int randomNumberGenerator(int length) throws Exception {
+    public static void randomNumberGenerator(int length){
         Random random = new Random(System.nanoTime());
         if(length > 10){
-            throw new Exception("Error: can't generate a secret number with a length of" + length
+            System.out.println("Error: can't generate a secret number with a length of " + length
                     + " because there aren't enough unique digits.");
+        }else{
+            int temp_number;
+            StringBuilder number = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                temp_number = random.nextInt(10);
+                while (!checkUniqueness(number, temp_number)){
+                    temp_number = random.nextInt(10);
+                }
+                number.append(temp_number);
+            }
+            System.out.println("The random secret number is " + number);
         }
-        int temp_number = 0;
-        StringBuilder number = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            temp_number = random.nextInt(10);
-            number.append(temp_number);
-        }
-        System.out.println("The random secret number is " + number);
-        return Integer.parseInt(number.toString());
     }
 
-
+    public static boolean checkUniqueness(StringBuilder number, int newNumber){
+        if (number.length() == 0){
+            return true;
+        } else if(number.charAt(0) == '0'){
+            return false;
+        }
+        int temp;
+        for (int i = 0; i < number.length(); i++) {
+            temp = number.charAt(i) - '0';
+            if (temp == newNumber){
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 
